42ee80f249da176c6f7db68485afa9d61219b42a
klozz <carlosj@klozz.dev>
2022-12-09 13:36:28 -0600

    davinci: add xperience makefiles
    Signed-off-by: klozz <carlosj@klozz.dev>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_davinci.mk
+    $(LOCAL_DIR)/xperience_davinci.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_davinci-user \
-    lineage_davinci-userdebug \
-    lineage_davinci-eng
+    xperience_davinci-user \
+    xperience_davinci-userdebug \
+    xperience_davinci-eng
